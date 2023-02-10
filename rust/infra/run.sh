56f883e342ba95552772a6beea7cbd9fdbb38289
tiramister <nh111424@gmail.com>
2023-02-10 16:56:52 +0900

    Docker の実行スクリプトを改修

--- rust/infra/run.sh
+++ rust/infra/run.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 if [ ${PWD##*/} != 'infra' ]; then
-  echo "[ERROR] Run this script in the infra directory."
+  echo '[ERROR] Run this script in the infra directory.'
   exit 1
 fi
+
+image_name='tiramister/rust-cp-workspace'
+container_name='rust-cp-container'
  
-if ! docker container ls -a | grep -q 'cp-rust-container'; then
+if ! docker image ls -a | grep -q $image_name; then
+  docker pull $image_name
+fi
+if ! docker container ls -a | grep -q $container_name; then
   docker container create \
-    --name cp-rust-container \
-    --mount type="bind",source="$(dirname ${PWD})",target="/home/tiramister/workspace/" \
-    cp-rust-image
+    --name $container_name \
+    --mount type=bind,source=$(dirname $PWD),target='/home/tiramister/workspace/' \
+    $image_name
 fi
 
-docker container start cp-rust-container
-docker container exec -it --env "TERM=xterm-256color" cp-rust-container /bin/zsh
-docker container stop cp-rust-container
+docker container start $container_name
+docker container exec -it --env 'TERM=xterm-256color' $container_name /bin/zsh
+docker container stop $container_name
 
